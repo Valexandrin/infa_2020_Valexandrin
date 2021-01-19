013cef1b19b241b026d40574c38d40005e8bfec8
Vadim Alexandrin <alex-vadim@mail.ru>
2021-01-19 10:41:57 +0300

    Timer was added

--- Catch_ball/Moving_ball_catcher.py
+++ Catch_ball/Moving_ball_catcher.py
@@ -4,13 +4,15 @@ from random import randint
 
 pygame.init()
 
-FPS = 20
+FPS = 18
 
 screen = pygame.display.set_mode((1200, 900))
 right = screen.get_rect().right
 left = screen.get_rect().left
 top = screen.get_rect().top
 bottom = screen.get_rect().bottom
+center_x = screen.get_rect().centerx
+print(center_x)
 
 font_name = pygame.font.match_font('arial', 1)
 
@@ -30,14 +32,29 @@ def balls_creation():
         for each ball put into a list '''
     global A
     A = []
-    for i in range(5):
-        x = randint(left + 100, right - 100)
-        y = randint(top + 100, bottom - 100)
-        r = randint(10, 100)
-        step_x = randint(-30, 30)
-        step_y = randint(-30, 30)
-        color = COLORS[randint(0, 5)]
-        A = A + [(x, y, r, step_x, step_y, color)]
+    for i in range(5):        
+        A = A + [update_param()]
+
+def update_param():
+    x = randint(left + 100, right - 100)
+    y = randint(top + 100, bottom - 100)
+    r = randint(25, 50)  
+    step_x = update_step(0)
+    step_y = update_step(0)
+    color = COLORS[randint(0, 5)]
+    new_param = x, y, r, step_x, step_y, color
+    return new_param
+
+def update_step(step):
+    ''' Function updates ball's velocity '''
+    if step == 0:
+        step = randint(-30, 30)
+
+    if step < 0:
+        step = randint(5, 30)
+    else:
+        step = randint(-30, -5)
+    return step
      
 def new_ball():
     ''' Function draws balls by the list of parameters '''    
@@ -45,64 +62,43 @@ def new_ball():
         circle(screen, color, (x, y), r)
 
 def coords_update():
-    ''' Function updates the list of parameters for each ball '''
-    global A
-    B = []    
-    for x, y, r, step_x, step_y, color in A:  
-        x, y, step_x, step_y, color = check_frame_hit(x, y, r,
-                                                      step_x, step_y, color)
-        B = B + [(x, y, r, step_x, step_y, color)]   
-    A = []   
-    A = A + B
-
-def check_frame_hit(x, y, r, step_x, step_y, color):
-    ''' Function checks if a ball hit screen frame and return updated
-        parameters: coordinates and velocityб color '''
-    if x - r + step_x <= left or x + r + step_x >= right:
-        step_x = update_step(step_x)
-        step_y = randint(-30, 30)
+    ''' Function checks for each ball if it hits screen frame and return
+        updated list of parameters like coordinates, velocity and color '''          
+    for set_param in A:
+        x, y, r, step_x, step_y, color = set_param
+        if x - r + step_x <= left or x + r + step_x >= right:
+            step_x = update_step(step_x)        
+            step_y = update_step(0)
+        elif y - r + step_y <= top or y + r + step_y >= bottom:        
+            step_x = update_step(0)
+            step_y = update_step(step_y)               
         x += step_x
         y += step_y
-        color = COLORS[randint(0, 5)]
-    elif y - r + step_y <= top or y + r + step_y >= bottom:
-        step_x = randint(-30, 30)
-        step_y = update_step(step_y)
-        x += step_x
-        y += step_y
-        color = COLORS[randint(0, 5)]
-    else:
-        x += step_x
-        y += step_y
-    return x, y, step_x, step_y, color
-
-def update_step(step):
-    ''' Function updates ball's velocity '''
-    if step < 0:
-        step = randint(1, 30)
-    else:
-        step = randint(-30, 1)
-    return step
+        A[A.index(set_param)] = [x, y, r, step_x, step_y, color]
 
 def hit_counter(score, event):
     ''' Func handles mouse click and counts quantity of success hits to ball '''
-    for x, y, r, step_x, step_y, color in A:
+    for set_param in A:
+        x, y, r, step_x, step_y, color = set_param        
         dist = ((x - event[0])**2 + (y - event[1])**2) ** (1/2)
         if r >= dist:
             score += 1
+            circle(screen, color, (x, y), r + 20, 5) #mark place of success hit
+            A[A.index(set_param)] = update_param()            
     return score
 
-def draw_text(surf, text, size):
+def draw_text(surf, text, pos_on_sceen, size):
     ''' Func draws text at top of game display '''
     font = pygame.font.Font(font_name, size)
-    text = font.render(text, True, WHITE)
-    textpos = text.get_rect()
-    textpos.centerx = surf.get_rect().centerx
-    surf.blit(text, textpos)
+    text_on_surf = font.render(text, True, WHITE)
+    textpos = text_on_surf.get_rect()
+    textpos.centerx = pos_on_sceen
+    surf.blit(text_on_surf, textpos)
     
 pygame.display.update()
 clock = pygame.time.Clock()
-score = 0
 balls_creation()
+score = 0
 finished = False
 pygame.mouse.set_visible(False)
 cursor_img = pygame.image.load('cursor_img.png')
@@ -114,15 +110,19 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:            
-            score = hit_counter(score, event.pos)          
+            score = hit_counter(score, event.pos)            
             print(score)
-    
-    draw_text(screen, 'Hits - '+str(score), 25)
+
+    seconds = 15 - (pygame.time.get_ticks())//1000
+    if seconds == 0:
+        break
+    draw_text(screen, 'Hits - '+str(score)+'  ('+str(seconds)+')', center_x, 25)
     new_ball()
     coords_update()
     cursor_img_rect.center = pygame.mouse.get_pos()
-    screen.blit(cursor_img, cursor_img_rect)    
+    screen.blit(cursor_img, cursor_img_rect)
     pygame.display.update()
     screen.fill(BLACK)
+
 
 pygame.quit()
